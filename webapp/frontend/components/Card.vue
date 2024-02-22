1622d972e76a971e99c7e243272bedb2e55db793
boa <boa@protonmail.com>
2024-02-22 15:45:05 +0100

    improve ui slightly

--- webapp/frontend/components/Card.vue
+++ webapp/frontend/components/Card.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="p-4">
         <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4 p-4">
-            <div class="font-bold text-xl mb-2">{{ article.title }}</div>
+            <p><a :href="article.url" class="font-bold text-xl mb-2">{{ article.title }}</a></p>
+            <UBadge color="blue"> {{ article.url.split('/')[2] }} </UBadge>
             <p class="text-base">{{ article.description }}</p>
             <p class="text-base">{{ article.date }}</p>
+
             <UContainer class="flex justify-between px-0 sm:px-0 lg:px-0">
-                <UButton color="red">Töröl</UButton>
+                <UButton color="red" @click="deleteArticle">Elutasít</UButton>
                 <UButton @click="openModal">Tovább</UButton>
             </UContainer>
         </div>
@@ -20,7 +22,7 @@
                 <SelectMenu class="my-2" :data="article.tags" :positive-data="article.tags" />
                 <UContainer class="my-2 flex justify-between px-0 sm:px-0 lg:px-0">
                     <UButton color="gray" @click="closeModal">Mégse</UButton>
-                    <UButton @click="submitArticle">Küldés</UButton>
+                    <UButton @click="submitArticle">Elfogad</UButton>
                 </UContainer>
             </div>
         </UModal>
@@ -28,6 +30,11 @@
 </template>
 
 <script setup lang="ts">
+    async function postUrl(url, data) {
+        //return await $fetch(url)
+        return await $fetch('https://corsproxy.io/?' + encodeURIComponent(url), data);
+    }
+
     function openModal() {
         isOpen.value = true
     }
@@ -35,13 +42,13 @@
         isOpen.value = false
     }
     async function deleteArticle() {
-        await $fetch('http://kmonitordemo.duckdns.org/api/not_corruption', {
+        await postUrl('http://kmonitordemo.duckdns.org/api/not_corruption', {
             method: 'POST',
             body: {'id': article.id}
         });
     }
     async function submitArticle() {
-        await $fetch('http://kmonitordemo.duckdns.org/api/annote', {
+        await postUrl('http://kmonitordemo.duckdns.org/api/annote', {
             method: 'POST',
             body: {
                 'id': article.id,
