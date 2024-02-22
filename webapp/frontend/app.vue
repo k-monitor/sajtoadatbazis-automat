de588b72859e4cdf9e222314a23cdd7e30c90fa2
boa <boa@protonmail.com>
2024-02-22 11:08:32 +0100

    implement new article ui

--- webapp/frontend/app.vue
+++ webapp/frontend/app.vue
@@ -1,33 +1,61 @@
 <script setup lang="ts">
     const items = [{
-        label: 'Tab1',
-        content: 'This is the content shown for Tab1'
+        label: 'Vegyes',
+        key: 'mixed'
     }, {
-    label: 'Tab2',
-        disabled: true,
-        content: 'And, this is the content for Tab2'
+        label: 'Elfogadott',
+        key: 'positive'
     }, {
-        label: 'Tab3',
-        content: 'Finally, this is the content for Tab3'
+        label: 'Elutasított',
+        key: 'negative'
     }]
 
+    var hostUrl = 'kmonitordemo.duckdns.org'
+
     const page = ref(1)
-    const response = await $fetch('http://kmonitordemo.duckdns.org/api/articles?page='+page.value);
+    let response = await $fetch('http://'+hostUrl+'/api/articles?page='+page.value);
     let articles = response.articles;
     let pages = response.pages;
     let itemsCount = ref(pages*10)
 
+    let newUrl = ''
+    let isOpen = ref(false)
+
     async function update() {
-        const response = await $fetch('http://kmonitordemo.duckdns.org/api/articles?page='+page.value);
+        const response = await $fetch('http://'+hostUrl+'/api/articles?page='+page.value);
         articles = response.articles;
         pages = response.pages;
         itemsCount = ref(pages*10)
     }
+
+    function openNewUrl () {
+        newUrl = ''
+        isOpen.value = true
+    }
+
+    async function addUrl () {
+        isOpen.value = false
+        await $fetch('http://kmonitordemo.duckdns.org/api/add_url', {
+            method: 'POST',
+            body: {'url': newUrl}
+        });
+    }
 </script>
 
 <template>
-    <div class="">
-        <Card class="flex justify-center" v-for="article in articles" :key="article.id" :article=article />
-        <UPagination class="p-4 justify-center" v-model="page" :page-count="10" :total="itemsCount" @click="update" />
-    </div>
+    <UButton @click="openNewUrl">Új cikk</UButton>
+    <UModal v-model="isOpen">
+        <div class="p-4">
+            <p>Új cikk</p>
+            <UInput class="my-2" v-model="newUrl" placeholder="https://telex.hu/..."/>
+            <UButton @click="addUrl">Hozzáad</UButton>
+        </div>
+    </UModal>
+
+    <UTabs :items="items" class="w-full">
+        <template #item="{ item }">
+            <Card class="flex justify-center" v-if="item.key === 'mixed'" v-for="article in articles" :key="article.id" :article=article />
+            <UPagination class="p-4 justify-center" v-if="item.key === 'mixed'" v-model="page" :page-count="10" :total="itemsCount" @click="update" />
+        </template>
+    </UTabs>
 </template>
