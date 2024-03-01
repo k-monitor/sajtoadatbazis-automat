913c6041309009627f38226b391859730aef1fd3
boa <boa@protonmail.com>
2024-03-01 16:18:42 +0100

    prepare mysql db backend

--- webapp/auto_kmdb/db.py
+++ webapp/auto_kmdb/db.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import os
 
 db = SQLAlchemy()
+
+mysql_db = mysql.connector.connect(
+  host=os.environ["MYSQL_HOST"],
+  port=os.environ["MYSQL_PORT"],
+  user=os.environ["MYSQL_USER"],
+  password=os.environ["MYSQL_PASS"],
+  database=os.environ["MYSQL_DB"],
+)
+
