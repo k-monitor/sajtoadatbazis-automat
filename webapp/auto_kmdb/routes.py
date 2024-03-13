e2e30f33efe50d2b1bc2d306edc9e0a237e97592
boa <boa@protonmail.com>
2024-03-13 11:46:10 +0100

    fix backend api

--- webapp/auto_kmdb/routes.py
+++ webapp/auto_kmdb/routes.py
@@ -1,10 +1,11 @@
 from flask import jsonify, Blueprint, request
-from auto_kmdb.db import get_articles, annote_negative
+from auto_kmdb.db import get_articles, annote_negative, connection_pool
 from auto_kmdb.db import get_all_persons, get_all_institutions, get_all_places, get_all_others, get_all_newspapers
 from auto_kmdb.db import check_url_exists, init_news
 from math import ceil
 
 api = Blueprint('api', __name__, url_prefix='/api')
+connection = connection_pool.get_connection()
 
 
 @api.route('/articles', methods=["GET"])
@@ -13,7 +14,7 @@ def api_articles():
     status = request.args.get('status', 'mixed', type=str)
     domain = request.args.get('domain', 'mind', type=str)
 
-    length, articles = get_articles(page, status, domain)
+    length, articles = get_articles(connection, page, status, domain)
 
     return jsonify({'pages': ceil(length/10), 'articles': articles}), 200
 
@@ -21,7 +22,7 @@ def api_articles():
 @api.route('/not_corruption', methods=["POST"])
 def not_corruption():
     id = request.json['id']
-    annote_negative(id)
+    annote_negative(connection, id)
     return jsonify({}), 200
 
 
@@ -35,18 +36,18 @@ def annote():
 @api.route('/add_url', methods=["POST"])
 def add_url():
     url = request.json['url']
-    if check_url_exists(url):
+    if check_url_exists(connection, url):
         return jsonify({'error': 'Cikk már létezik'}), 400
-    init_news(1, url, url)
+    init_news(connection, 1, url, url)
     return jsonify({}), 200
 
 
 @api.route('/all_labels', methods=["GET"])
 def all_labels():
     return jsonify({
-        'person': get_all_persons(),
-        'institution': get_all_institutions(),
-        'place': get_all_places(),
-        'keywords': get_all_others(),
-        'domains': get_all_newspapers(),
+        'person': get_all_persons(connection),
+        'institution': get_all_institutions(connection),
+        'place': get_all_places(connection),
+        'keywords': get_all_others(connection),
+        'domains': get_all_newspapers(connection),
     }), 200
