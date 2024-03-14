6c41d27d7a30bdddac2e152c30b65165a0d6446e
boa <boa@protonmail.com>
2024-03-14 12:25:36 +0100

    implement retrieving tags for articles

--- webapp/auto_kmdb/db.py
+++ webapp/auto_kmdb/db.py
@@ -2,6 +2,7 @@ from functools import cache
 import mysql.connector
 import os
 from contextlib import closing
+import datetime
 
 
 connection_pool = mysql.connector.pooling.MySQLConnectionPool(
@@ -18,18 +19,6 @@ connection_pool = mysql.connector.pooling.MySQLConnectionPool(
 
 @cache
 def get_all(connection, table, id_column, name_column, limit=300):
-    """
-    This function retrieves a limited number of records from a specified MySQL table where the status is 'Y'.
-
-    Args:
-        table (str): The name of the table from which to retrieve records.
-        id_column (str): The name of the column to be used as the ID in the retrieved records.
-        name_column (str): The name of the column to be used as the name in the retrieved records.
-        limit (int, optional): The maximum number of records to retrieve. Defaults to 3.
-
-    Returns:
-        list: A list of dictionaries, where each dictionary represents a record from the table and contains the ID and name.
-    """
     query = f'SELECT {id_column} AS id, {name_column} AS name FROM {table} WHERE status = "Y" LIMIT {limit};'
     with connection.cursor(dictionary=True) as cursor:
         cursor.execute(query)
@@ -57,25 +46,12 @@ def get_all_newspapers(connection):
 
 
 def init_news(connection, source, source_url, clean_url):
-    """
-    This function is used to insert news data into the 'autokmdb_news' table in the MySQL database.
-
-    Parameters:
-    source (str): The source from where the news is obtained.
-    added_by (str): The person who added the news.
-    source_url (str): The original URL of the news.
-    clean_url (str): The sanitized URL of the news.
-    processing_step (str): The current step in the processing pipeline.
-
-    Returns:
-    None
-    """
-
+    current_datetime = datetime.now()
     with connection.cursor(dictionary=True) as cursor:
         query = """INSERT INTO autokmdb_news
-                (source, source_url, clean_url, processing_step)
+                (source, source_url, clean_url, processing_step, cre_time)
                 VALUES (%s, %s, %s, %s)"""
-        cursor.execute(query, (0 if source == 'rss' else 1, source_url, clean_url, 0))
+        cursor.execute(query, (0 if source == 'rss' else 1, source_url, clean_url, 0, current_datetime.strftime("%Y-%m-%d %H:%M:%S")))
     connection.commit()
 
 
@@ -89,21 +65,6 @@ def check_url_exists(connection, url):
 
 
 def add_auto_person(connection, autokmdb_news_id, person_id, found_name, found_position, name, classification_score, classification_label):
-    """
-    This function adds a person to the 'autokmdb_persons' table in the MySQL database.
-
-    Parameters:
-    autokmdb_news_id (int): The ID of the news article from the autokmdb_news where the person was found.
-    person_id (int): The unique ID of the person.
-    found_name (str): The name of the person as found in the news article.
-    found_position (str): The position of the person as found in the news article.
-    name (str): The actual name of the person.
-    classification_score (float): The score given by the classification model for the person.
-    classification_label (str): The label given by the classification model for the person.
-
-    Returns:
-    None
-    """
     with connection.cursor() as cursor:
         query = """INSERT INTO autokmdb_persons 
                 (autokmdb_news_id, person_id, found_name, found_position, name, classification_score, classification_label) 
@@ -113,21 +74,6 @@ def add_auto_person(connection, autokmdb_news_id, person_id, found_name, found_p
 
 
 def add_auto_institution(connection, autokmdb_news_id, institution_id, found_name, found_position, name, classification_score, classification_label):
-    """
-    This function adds an institution to the 'autokmdb_institutions' table in the MySQL database.
-
-    Parameters:
-    autokmdb_news_id (int): The ID of the news article from the autokmdb_news where the institution was found.
-    institution_id (int): The unique ID of the institution.
-    found_name (str): The name of the institution as found in the news article.
-    found_position (str): The position of the institution as found in the news article.
-    name (str): The actual name of the institution.
-    classification_score (float): The score given by the classification model for the institution.
-    classification_label (str): The label given by the classification model for the institution.
-
-    Returns:
-    None
-    """
     with connection.cursor() as cursor:
         query = """INSERT INTO autokmdb_institutions 
                 (autokmdb_news_id, institution_id, found_name, found_position, name, classification_score, classification_label) 
@@ -137,21 +83,6 @@ def add_auto_institution(connection, autokmdb_news_id, institution_id, found_nam
 
 
 def add_auto_place(connection, autokmdb_news_id, place_id, found_name, found_position, name, classification_score, classification_label):
-    """
-    This function adds a place to the 'autokmdb_places' table in the MySQL database.
-
-    Parameters:
-    autokmdb_news_id (int): The ID of the news article from the autokmdb_news where the place was found.
-    place_id (int): The unique ID of the place.
-    found_name (str): The name of the place as found in the news article.
-    found_position (str): The position of the place as found in the news article.
-    name (str): The actual name of the place.
-    classification_score (float): The score given by the classification model for the place.
-    classification_label (str): The label given by the classification model for the place.
-
-    Returns:
-    None
-    """
     with connection.cursor() as cursor:
         query = """INSERT INTO autokmdb_places
                 (autokmdb_news_id, place_id, found_name, found_position, name, classification_score, classification_label)
@@ -161,21 +92,6 @@ def add_auto_place(connection, autokmdb_news_id, place_id, found_name, found_pos
 
 
 def add_auto_other(connection, autokmdb_news_id, other_id, found_name, found_position, name, classification_score, classification_label):
-    """
-    This function adds an 'other' to the 'autokmdb_others' table in the MySQL database.
-
-    Parameters:
-    autokmdb_news_id (int): The ID of the news article from the autokmdb_news where the 'other' was found.
-    other_id (int): The unique ID of the 'other'.
-    found_name (str): The name of the 'other' as found in the news article.
-    found_position (str): The position of the 'other' as found in the news article.
-    name (str): The actual name of the 'other'.
-    classification_score (float): The score given by the classification model for the 'other'.
-    classification_label (str): The label given by the classification model for the 'other'.
-
-    Returns:
-    None
-    """
     with connection.cursor() as cursor:
         query = """INSERT INTO autokmdb_others
                 (autokmdb_news_id, other_id, found_name, found_position, name, classification_score, classification_label)
@@ -209,20 +125,6 @@ def save_classification_step(connection, id, classification_label, classificatio
 
 
 def get_step_queue(connection, step):
-    """
-    This function fetches the data from the 'autokmdb_news' table 
-    in the database where the processing_step is the given step. It returns the result as a list of dictionaries.
-
-    Args:
-    step (int): The processing step to fetch data for.
-
-    Returns:
-    list: A list of dictionaries where each dictionary represents a row from the query result.
-    Each dictionary contains 'id' and the relevant data as keys.
-
-    Example:
-    [{'id': 1, 'url': 'http://example.com'}, {'id': 2, 'url': 'http://example2.com'}, ...]
-    """
     fields = {
         0: 'clean_url AS url',
         1: 'title, description',
@@ -269,23 +171,31 @@ def get_articles(connection, page, status, domain='mind'):
     else:
         domain = f"%{domain}%"
 
-    selection = 'SELECT id, clean_url, description, title, source, classification_score, text FROM autokmdb_news '
+    selection = '''SELECT n.id AS id, clean_url AS url, description, title, source, n.classification_score AS classification_score,
+            n.text AS text, n.cre_time AS date, GROUP_CONCAT(p.id SEPARATOR ',') AS persons, GROUP_CONCAT(i.id SEPARATOR ',') AS institutions,
+            GROUP_CONCAT(o.id SEPARATOR ',') AS others
+        FROM autokmdb_news n
+        LEFT JOIN autokmdb_persons p ON n.id = p.autokmdb_news_id 
+        LEFT JOIN autokmdb_institutions i ON n.id = i.autokmdb_news_id 
+        LEFT JOIN autokmdb_others o ON n.id = o.autokmdb_news_id 
+        '''
+    group = ' GROUP BY id'
 
     if status == 'mixed':
-        query = '''WHERE classification_label = 1 AND processing_step = 3 AND annotation_label IS NULL AND clean_url LIKE %s'''
+        query = '''WHERE n.classification_label = 1 AND processing_step = 3 AND n.annotation_label IS NULL AND clean_url LIKE %s'''
     elif status == 'positive':
-        query = '''WHERE classification_label = 1 AND processing_step = 5 AND annotation_label = 1 AND clean_url LIKE %s'''
+        query = '''WHERE n.classification_label = 1 AND processing_step = 5 AND n.annotation_label = 1 AND clean_url LIKE %s'''
     elif status == 'negative':
-        query = '''WHERE classification_label = 1 AND processing_step = 5 AND annotation_label = 0 AND clean_url LIKE %s'''
+        query = '''WHERE n.classification_label = 1 AND processing_step = 5 AND n.annotation_label = 0 AND clean_url LIKE %s'''
     else:
         print('Invalid status provided!')
         return
 
     with connection.cursor(dictionary=True) as cursor:
-        cursor.execute('SELECT COUNT(*) FROM autokmdb_news '+query, (domain,))
-        count = cursor.fetchone()
-        cursor.execute(paginate_query(selection + query, 10, page), (domain,))
-        return count['COUNT(*)'], cursor.fetchall()
+        cursor.execute('SELECT COUNT(id) FROM autokmdb_news n '+query, (domain,))
+        count = cursor.fetchone()['COUNT(id)']
+        cursor.execute(paginate_query(selection + query + group, 10, page), (domain,))
+        return count, cursor.fetchall()
 
 
 def annote_negative(connection, id):
