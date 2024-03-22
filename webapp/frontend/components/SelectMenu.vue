89f4246e43f0d5462d757d047d9504a01f4e1f21
boa <boa@protonmail.com>
2024-03-22 16:41:32 +0100

    add percentages, fix search

--- webapp/frontend/components/SelectMenu.vue
+++ webapp/frontend/components/SelectMenu.vue
@@ -2,11 +2,15 @@
     <!--  multiple by="label" option-attribute="label" @update:model-value="$emit('update:positiveList', localPositiveList)" -->
 
     <p style="text-transform: capitalize;"> {{ type }}: </p>
-    <USelectMenu 
-        @close="() => $emit('update:positiveList', localPositiveList)"
-        creatable :searchable="search" searchable-placeholder="Keresés..."
-        class="my-2"
-        v-model="localPositiveList" :options="localList" by="id" option-attribute="db_name" multiple>
+    <USelectMenu
+            @close="() => $emit('update:positiveList', localPositiveList)"
+            :searchable="search"
+            searchable-placeholder="Keresés..."
+            class="my-2"
+            v-model="localPositiveList" :options="localList" by="id" 
+            creatable
+            multiple
+        >
         <template #label>
             <span v-if="localPositiveList.length" class="truncate">{{ localPositiveList.map((item) => item.db_name != 'null' ? item.db_name : item.name).join(', ') }}</span>
             <span v-else>Válassz ki elemeket</span>
@@ -17,11 +21,10 @@
         </template>
         <template #option="{ option }">
             <div v-if="option.db_name != 'null'">
-                
-                <span class="block truncate"> <Icon name="mdi:database-outline" color="green" /> {{ option.db_name }}</span>
+                <span class="block truncate"> <Icon name="mdi:database-outline" color="green" /> {{ option.db_name }}  {{option.classification_score != null ? '('+(option.classification_score*100).toFixed(0)+'%)' : ''}} </span>
             </div>
             <div v-else>
-                <span class="block truncate">{{ option.name }}</span>
+                <span class="block truncate">{{ option.name }} {{option.classification_score != null ? '('+(option.classification_score*100).toFixed(0)+'%)' : ''}}</span>
             </div>
         </template>
         <template #empty>
@@ -37,8 +40,8 @@
         }
 
         return labels.filter((item: any) => {
-            return item.name.toLowerCase().includes(q.toLowerCase())
-        })
+            return item.name != null && item.name.toLowerCase().includes(q.toLowerCase())
+        }).map((item: any) => {return {'id': item.id, 'name': item.name, 'db_name': item.name}})
     }
 
     var { list, positiveList, labels, type } = defineProps(['list', 'positiveList', 'labels', 'type']);
