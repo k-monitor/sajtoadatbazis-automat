5ead274ab6e2cbeb31d6d301b3aa68ef40cd209b
boa <boa@protonmail.com>
2024-04-13 12:26:39 +0200

    fix showing buttons

--- webapp/frontend/components/Card.vue
+++ webapp/frontend/components/Card.vue
@@ -14,7 +14,7 @@
             <p class="text-base text-pretty">{{ article.description }}</p>
             <p class="text-base text-right py-1">{{ article.date }}</p>
 
-            <UContainer v-if="article.processing_step >= 5" class="flex justify-between px-0 sm:px-0 lg:px-0">
+            <UContainer v-if="article.processing_step >= 4" class="flex justify-between px-0 sm:px-0 lg:px-0">
                 <UButton v-if="article.annotation_label != 0" color="red" @click="deleteArticle">{{ article.annotation_label == null ? "Elutasít" : "Kiszed" }}</UButton>
                 <UButton v-if="true" @click="openModal" class="ml-auto">{{ article.annotation_label == null ? "Tovább" : "Szerkeszt" }}</UButton>
             </UContainer>
