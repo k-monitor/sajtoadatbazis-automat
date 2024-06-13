473717a496da019dfe550dea12c091c7f66166b6
boa <boa@protonmail.com>
2024-06-13 13:23:03 +0200

    add datepicker to frontend

--- webapp/frontend/app.vue
+++ webapp/frontend/app.vue
@@ -3,7 +3,22 @@
     colorMode: 'light',
     })
 
-    let newUrl = '';
+    import { sub, format, isSameDay, type Duration } from 'date-fns'
+
+    const ranges = [
+    { label: 'Last 7 days', duration: { days: 7 } },
+    { label: 'Last 14 days', duration: { days: 14 } },
+    { label: 'Last 30 days', duration: { days: 30 } },
+    { label: 'Last 3 months', duration: { months: 3 } },
+    { label: 'Last 6 months', duration: { months: 6 } },
+    { label: 'Last year', duration: { years: 1 } }
+    ]
+    const selected = ref({ start: sub(new Date(), { days: 14 }), end: new Date() })
+
+    function isRangeSelected (duration: Duration) {
+        return isSameDay(selected.value.start, sub(new Date(), duration)) && isSameDay(selected.value.end, new Date())
+    }
+        let newUrl = '';
     let isOpen = ref(false);
     let isOpenError = ref(false);
     let errorText = ref('');
@@ -59,6 +74,8 @@
             page: page,
             status: status,
             domain: selectedDomain,
+            from: format(selected.value.start, 'yyyy-MM-dd'),
+            to: format(selected.value.end, 'yyyy-MM-dd'),
             q: q,
         },
         onResponse({ request, response, options }) {
@@ -145,6 +162,32 @@
             <UButton class="mr-1" @click="openNewUrl">Új cikk</UButton>
             <div>
             <UContainer class="my-1 flex lg:px-0 px-4 sm:px-0 ml-1">
+                <UPopover :popper="{ placement: 'bottom-start' }">
+                    <UButton icon="i-heroicons-calendar-days-20-solid">
+                    {{ format(selected.start, 'yyyy. MM. dd.') }} - {{ format(selected.end, 'yyyy. MM. dd.') }}
+                    </UButton>
+
+                    <template #panel="{ close }">
+                    <div class="flex items-center sm:divide-x divide-gray-200 dark:divide-gray-800">
+                        <!-- <div class="hidden sm:flex flex-col py-4">
+                        <UButton
+                            v-for="(range, index) in ranges"
+                            :key="index"
+                            :label="range.label"
+                            color="gray"
+                            variant="ghost"
+                            class="rounded-none px-6"
+                            :class="[isRangeSelected(range.duration) ? 'bg-gray-100 dark:bg-gray-800' : 'hover:bg-gray-50 dark:hover:bg-gray-800/50']"
+                            truncate
+                            @click="selectRange(range.duration)"
+                        />
+                        </div> -->
+
+                        <DatePicker v-model="selected" is-required @close="() => {close(); refresh()}" />
+                    </div>
+                    </template>
+                </UPopover>
+
                 <p>Kiválasztott hírportál: &nbsp;</p>
                 <UInputMenu class="w-48" v-model="selectedDomain" option-attribute="name" value-attribute="id" :options="allDomains" @change="refresh">
                     <template #option="{ option }">
