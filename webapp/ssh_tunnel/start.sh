7904c519fb4a6b1daac79372c0a0eb1eb5b90b98
boa <boa@protonmail.com>
2024-07-18 08:01:15 +0200

    fix proxy setup

--- webapp/ssh_tunnel/start.sh
+++ webapp/ssh_tunnel/start.sh
@@ -4,7 +4,7 @@
 ssh -N -L 9999:127.0.0.1:3306 -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu -v &
 
 # Start the SOCKS5 proxy
-ssh -D 1080 -f -C -q -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no autokmdb@ahalo.hu
+ssh -D 1080 -f -C -q -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu -v
 
 # Keep the container running
 tail -f /dev/null
