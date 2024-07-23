5c3b5a21f18aa897c025113e6bb0b2bd2ae98fdb
boa <boa@protonmail.com>
2024-07-23 20:42:00 +0200

    remove unnecessary verbose

--- webapp/ssh_tunnel/start.sh
+++ webapp/ssh_tunnel/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Start the SSH tunnel
-ssh -N -L 9999:127.0.0.1:3306 -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu -v &
+ssh -N -L 9999:127.0.0.1:3306 -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu &
 
 # Start the SOCKS5 proxy
-ssh -D 1080 -f -C -q -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu -v
+ssh -D 1080 -f -C -q -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu
 
 # Keep the container running
 tail -f /dev/null
