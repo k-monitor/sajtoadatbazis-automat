7023fcbf58f5368b7d3ea36b560f33b5debe90e7
boa <boa@protonmail.com>
2024-07-31 13:16:32 +0200

    clean up some files

--- webapp/auto_kmdb/options.py
+++ webapp/auto_kmdb/options.py
@@ -9,5 +9,4 @@ def load_json_from_file(filename: str) -> dict:
             return {}
 
 
-feed_urls = load_json_from_file('auto_kmdb/data/feed_urls.json')
 skip_url_patterns = load_json_from_file('auto_kmdb/data/skip_url_patterns.json')
