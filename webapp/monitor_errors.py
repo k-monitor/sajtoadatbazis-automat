d83aaa2ef43d4d6899612802f5109e7177c3bcb3
boa <boa@protonmail.com>
2024-08-16 09:51:37 +0200

    improve error detection

--- webapp/monitor_errors.py
+++ webapp/monitor_errors.py
@@ -3,12 +3,12 @@ import re
 import subprocess
 
 log_file = "data/log.txt"
-error_pattern = r"ERROR:root:MySQL Connection not available."
+error_pattern = r"(ERROR:root:MySQL Connection not available)|(ERROR:root:Can not reconnect to MySQL)"
 
 
 def check_log_file():
     with open(log_file, 'r') as f:
-        lines = f.readlines()[-2:]
+        lines = f.readlines()[-10:]
         return any(re.search(error_pattern, line) for line in lines)
 
 
