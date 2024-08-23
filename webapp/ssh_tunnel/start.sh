bd940b7d9d2aa6cf3aea5a4c0404fb6d51554d05
boa <boa@protonmail.com>
2024-08-23 10:11:25 +0200

    increase logging of ssh container

--- webapp/ssh_tunnel/start.sh
+++ webapp/ssh_tunnel/start.sh
@@ -4,7 +4,7 @@
 ssh -N -L 9999:127.0.0.1:3306 -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu &
 
 # Start the SOCKS5 proxy
-ssh -D 1080 -f -C -q -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu
+ssh -vv -D 1080 -f -C -N -p 2267 -i /secrets/autokmdb_key -o PubkeyAcceptedKeyTypes=ssh-rsa -o StrictHostKeyChecking=no -o GatewayPorts=true autokmdb@ahalo.hu
 
 # Keep the container running
 tail -f /dev/null
