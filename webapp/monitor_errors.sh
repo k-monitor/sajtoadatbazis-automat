ba2c3bb2165f3b359c6eaea961d2d5972bdac4ab
Boa <17887448+boapps@users.noreply.github.com>
2024-10-25 11:47:05 +0200

    add logging restart incidents

--- webapp/monitor_errors.sh
+++ webapp/monitor_errors.sh
@@ -3,6 +3,15 @@
 check_for_errors() {
     result=$(podman-compose logs 2>&1)
     if [[ "${result}" == *"ERROR"* ]] || [[ "${result}" == *"CRITICAL"* ]] || [[ "${result}" == *"mysql.connector.errors.OperationalError"* ]]; then
+        # Get the current date and time in the desired format
+        current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+
+        # Define the output file name
+        output_file="podman_logs_$current_time.txt"
+
+        # Execute podman-compose logs and redirect the output to the file
+        echo result > "$output_file"
+
         return 0 # Return true
     else
         return 1 # Return false
