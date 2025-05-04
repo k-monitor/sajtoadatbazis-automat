cf8cf72f4bac8f538e36115de50056b8bcb6216f
boa <boa@protonmail.com>
2025-05-04 21:30:26 +0200

    fix blank screen error when labels are null

--- webapp/frontend/components/Card.vue
+++ webapp/frontend/components/Card.vue
@@ -69,12 +69,12 @@
           {{ Math.round(article.classification_score * 100) }}%
         </p>
         <UButton v-if="article.annotation_label == null && article.classification_label == 0" @click="processAndAccept"
-          :loading="accepting" class="">Feldolgoz és átsorol</UButton>
+          :loading="accepting" class="" :disabled="allLabels == null">Feldolgoz és átsorol</UButton>
         <UButton v-if="article.annotation_label == null && article.classification_label == 1" @click="openModal"
-          :loading="isOpening" class="">Szerkesztés</UButton>
-        <UButton v-if="article.annotation_label == 1" @click="openModal" :loading="isOpening" class="">Szerkesztés
+          :loading="isOpening" class="" :disabled="allLabels == null">Szerkesztés</UButton>
+        <UButton v-if="article.annotation_label == 1" @click="openModal" :loading="isOpening" :disabled="allLabels == null" class="">Szerkesztés
         </UButton>
-        <UButton v-if="article.annotation_label == 0" @click="openModal" :loading="isOpening" class="">Mégis elfogad
+        <UButton v-if="article.annotation_label == 0" @click="openModal" :loading="isOpening" :disabled="allLabels == null" class="">Mégis elfogad
         </UButton>
       </UContainer>
       <div class="flex justify-between" v-if="!is_small">
